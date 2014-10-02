9e416c04187867baee9eed1bab5d69b9353bb921
kimjoohwee <kjhwee91@gmail.com>
2014-10-02 15:28:46 +0900

    chart complete

--- script/dataController.js
+++ script/dataController.js
@@ -1,19 +1,61 @@
-function dataApply(e){
-	var dataSection = e.target.nextElementSibling;
-	var dataSet = dataSection.querySelectorAll("form");
-	FormData.extract(dataSet);
-}
+function DataController(){
 
-var FormData = {
-	extract : function(dataSet){
+}
+// k: "1월", v: 100, comment: "aaa"
+DataController.prototype = {
+	customizing : function(userRawDataArray){
+		var customizedDataMap = new Map();
+		var prev, next = null;
+		var nextPoint = 0;
+
+		userRawDataArray.map(function(item){
+			var next = userRawDataArray[++nextPoint];
+			var object = {
+				"value" : parseInt(item.v),
+				"order" : nextPoint - 1,
+				// "ratio" : item.v / totalValue,
+				"comment" : item.comment,
+				"prev" : prev!=null?prev.k:null,
+				"next" : next!=null?next.k:null
+			};
+			prev = item;
+			customizedDataMap.put(item.k, object);
+		}.bind(this));
+		return customizedDataMap;
+	},
+
+	getNeed : {
+		total : function(userRawDataArray){
+			var total = 0;
+			userRawDataArray.map(function(item){
+				total += item.v;
+			});
+			return total;
+		},
+
+		maximum : function(dataMap){
+			if(dataMap===undefined)
+				return 0;
+			var max = 0;
+			var dMap = dataMap.map;
+			for(key in dMap){
+				max = dMap[key].value>max?dMap[key].value:max;
+			}
+			return max;
+		},
+
+		dataLength : function(dataMap){
+			if(dataMap===undefined)
+				return 0;
+			return dataMap.size();
+		}
 	}
 }
 
-// var toUsingData = dataEditing(testDatas);
-// initialBackground(svgSetting, function(){
-// 	renderLineGraph(toUsingData, colorSet);
-// });
-// initialDataSet(toUsingData);
 
-// var dataApplyButton = document.querySelector("#dataApply");
-// dataApplyButton.addEventListener("click", dataApply);
+
+
+
+
+
+
